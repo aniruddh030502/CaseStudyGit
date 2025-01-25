44307ad2463cf1d9549b04e4a378aa5dc6f74b0c
aniruddh030502 <aniruddhadas371@gmail.com>
2025-01-25 15:25:55 +0530

    Dice is implemented.

---
+++ dice.java
@@ -0,0 +1,11 @@
+class Dice {
+    private final Random random;
+
+    public Dice() {
+        random = new Random();
+    }
+
+    public int roll() {
+        return random.nextInt(6) + 1;
+    }
+}
